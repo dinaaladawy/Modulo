3c41189b383f33e00f3a17268ba076eb209a3378
AndreiCostinescu <andrei.costinescu@yahoo.com>
2017-03-23 15:54:17 +0100

    Replaced some special characters in the database
    Changed some special characters in the database because of URL
    formatting when using views with django... (it makes it easier when
    using regular expressions)

--- App/JungeAkademie/cleanupNames.py
+++ App/JungeAkademie/cleanupNames.py
@@ -9,14 +9,41 @@ import django, os
 os.environ['DJANGO_SETTINGS_MODULE'] = "JungeAkademie.settings"
 django.setup()
 from modulo.models import Category, Interest, Module
+from openpyxl import load_workbook
 
 def cleanupString(s):
     #– -> -
     #„“ -> ""
-    s = s.replace("–", "-")
-    s = s.replace("„", "\"")
-    s = s.replace("“", "\"")
+    try:
+        s = s.replace("–", "-")
+        s = s.replace("„", "\"")
+        s = s.replace("“", "\"")
+        s = s.replace("”", "\"")
+        s = s.replace("’", "'")
+    except AttributeError:
+        #print(s, "is not a string.")
+        pass
     return s
+
+def cleanupExcel():
+    excelNames = ["interests", "categories", "modules"]
+    for name in excelNames:
+        excelFile = "./"+name+".xlsx"
+        workbook = load_workbook(excelFile)
+        worksheet = workbook[name]
+        nrRows = len(tuple(worksheet.rows))
+        nrCols = len(tuple(worksheet.columns))
+        counter = 0
+        for row in worksheet.iter_rows(min_row=1, min_col=1, max_row=nrRows, max_col=nrCols):
+            for cell in row:
+                cleanup = cleanupString(cell.value)
+                if cell.value != cleanup:
+                    print("Cleaned-up string:", cleanup, "\ncell.value: ", cell.value, "\n", sep='\n')
+                    cell.value = cleanup
+                    counter += 1
+        print("Cleaned-up %s entries in \'%s\' excel file" % (counter, excelFile))
+        if counter > 0:
+            workbook.save(filename=excelFile)
 	
 def cleanupModules():
     counter = 0
@@ -48,6 +75,7 @@ def cleanupCategories():
             counter += 1
     print("Cleaned-up %s categories." % counter)
         
+cleanupExcel()
 cleanupModules()
 cleanupInterests()
 cleanupCategories()
